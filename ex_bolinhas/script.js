a6f516855736761ad70ff21436968eccc84c9fc5
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-05-26 00:16:04 -0300

    html&css

---
+++ ex_bolinhas/script.js
@@ -0,0 +1,7 @@
+const palco=document.querySelector('#palco')
+const num_objetos=document.querySelector('#num_objetos')
+const txt_qtde=document.querySelector('#txt_qtde')
+const btn_add=document.querySelector('#btn_add')
+const btn_remover=document.querySelector('#btn_remover')
+
+console.log(palco, num_objetos, txt_qtde, btn_add, btn_remover)
