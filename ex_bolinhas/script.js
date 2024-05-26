728f6fae96217a491b70636283e4044d97bc08af
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-05-26 00:38:50 -0300

    Update script.js

--- ex_bolinhas/script.js
+++ ex_bolinhas/script.js
@@ -10,13 +10,36 @@ let bolas=[]
 let numBola=0
 
 class Bola{
-    constructor(){
+    constructor(arrayBolas,palco){
         this.tam=Math.floor(Math.random()*15)+10
         this.r=Math.floor(Math.random()*255)
         this.g=Math.floor(Math.random()*255)
         this.b=Math.floor(Math.random()*255)
-        this.tam=Math.floor(Math.random()*(larguraPalco-this.tam))
-        this.tam=Math.floor(Math.random()*(alturaPalco-this.tam))
+        this.px=Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.py=Math.floor(Math.random()*(alturaPalco-this.tam))
+        this.velx=Math.floor(Math.random()*2)+0.5
+        this.vely=Math.floor(Math.random()*2)+0.5
+        this.dirx=Math.random()*10>5?1:-1
+        this.dirx=Math.random()*10>5?1:-1
+        this.palco=palco
+        this.arrayBolas=arrayBolas
+        this.id=Date.now()+"_"+Math.floor(Math.random()*100000000000)
+        this.desenhar()
+        this.controle=setInterval(this.controlar, 10)
+        this.eu=document.getElementById(this.id)
+        //this.eu=document.querySelector(`#${this.id}`)
+    }
+    minhaPos=()=>{
+    
+    }
+    remover=()=>{
+
+    }
+    desenhar=()=>{
+
+    }
+    controlar=()=>{
+
     }
 }
 
