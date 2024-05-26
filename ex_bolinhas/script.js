1c7e356fc9de7ba739b4db24cf9ea7792b6ce68d
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-05-26 00:27:51 -0300

    instancing-class

--- ex_bolinhas/script.js
+++ ex_bolinhas/script.js
@@ -4,4 +4,36 @@ const txt_qtde=document.querySelector('#txt_qtde')
 const btn_add=document.querySelector('#btn_add')
 const btn_remover=document.querySelector('#btn_remover')
 
-console.log(palco, num_objetos, txt_qtde, btn_add, btn_remover)
+let larguraPalco=palco.offsetWidth
+let alturaPalco=palco.offsetHeight
+let bolas=[]
+let numBola=0
+
+class Bola{
+    constructor(){
+        this.tam=Math.floor(Math.random()*15)+10
+        this.r=Math.floor(Math.random()*255)
+        this.g=Math.floor(Math.random()*255)
+        this.b=Math.floor(Math.random()*255)
+        this.tam=Math.floor(Math.random()*(larguraPalco-this.tam))
+        this.tam=Math.floor(Math.random()*(alturaPalco-this.tam))
+    }
+}
+
+window.addEventListener('resize', ()=>{
+    larguraPalco=palco.offsetWidth
+    alturaPalco=palco.offsetHeight
+})
+
+btn_add.addEventListener('click', ()=>{
+    const qtde=txt_qtde.value
+    for(let i=0;I<qtde;i++){
+
+    }
+})
+
+btn_remover.addEventListener('click', ()=>{
+    bolas.map((b)=>{
+
+    })
+})
